f14ee65c5a5cbe8925b20d5fa66c179236c3c49c
Kel-Bacelar <95115026+Kel-Bacelar@users.noreply.github.com>
2022-08-30 19:35:48 -0300

    Add files via upload

---
+++ Atividade22_Softex.py
@@ -0,0 +1,15 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+class Alunos:
+    def __init__(self, nomeAluno, totalAlunos)
+    self.__nomeAluno = nomeAluno
+    self.__totalAlunos = totalAlunos
+    
+    def matricula(self.)
+    imput(str(print(f'Digite o nome do aluno: ')
+    self.__totalAlunos ++
+    
+    
+    
+    
